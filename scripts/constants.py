c5bf7e0f84cc7cf6a0cf399c2524dcc0d431965b
Hamza Shafiq <53669917+hshaf@users.noreply.github.com>
2023-09-21 18:53:16 -0400

    add paths to shapefile data for AZ and WI

--- scripts/constants.py
+++ scripts/constants.py
@@ -2,3 +2,5 @@
 
 # Paths to data files
 VA_DISTRICTS_PATH = "../data/va_cong_adopted_2021.zip"
+AZ_DISTRICTS_PATH = "../data/az_cong_adopted_2022.zip"
+WI_DISTRICTS_PATH = "../data/wi_cong_adopted_2022.zip"
