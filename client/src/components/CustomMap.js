c13cab17a7ff1e0e56f268fbf61e3da9372e913c
Hamza Shafiq <53669917+hshaf@users.noreply.github.com>
2023-09-29 07:48:21 -0400

    add custom map component that displays state district boundaries

---
+++ client/src/components/CustomMap.js
@@ -0,0 +1,20 @@
+import { MapContainer, TileLayer, useMap, Marker, Popup, GeoJSON } from 'react-leaflet';
+import VADistricts from '../data/va-districts.json';
+import AZDistricts from '../data/az-districts.json';
+// import WIDistricts from '../data/wi-districts.json';
+
+function CustomMap() {
+  return (
+    <MapContainer center={[38.86438706880524, -95.47162384288428]} zoom={5} scrollWheelZoom={true}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <GeoJSON weight={1} color='blue' key={'Virginia'} data={VADistricts} />
+        <GeoJSON weight={1} color='blue' key={'Arizona'} data={AZDistricts} />
+        {/* <GeoJSON weight={1} color='blue' key={'Wisconsin'} data={WIDistricts} /> */}
+    </MapContainer>
+  );
+}
+
+export default CustomMap;
--- client/src/components/DisplayPane.js
+++ client/src/components/DisplayPane.js
@@ -1,5 +1,5 @@
-import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet'
 import VisualComponent from "./VisualComponent";
+import CustomMap from './CustomMap';
 
 function DisplayPane () {
   return (
@@ -8,18 +8,8 @@ function DisplayPane () {
       <VisualComponent></VisualComponent>
       </div>
       
-      <div id="map">
-      <MapContainer center={[40.912758739378624, -73.12381229926775]} zoom={13} scrollWheelZoom={true}>
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <Marker position={[40.912758739378624, -73.12381229926775]}>
-          <Popup>
-            CSE 416
-          </Popup>
-        </Marker>
-      </MapContainer>
+      <div id="map-pane">
+      <CustomMap></CustomMap>
       </div>
     </div>
   );
