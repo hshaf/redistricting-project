00d119f5ac13f2447711a45ea7100fc3834f8b82
mapang02 <marcus.pang@stonybrook.edu>
2023-09-30 20:41:39 -0400

    Change tab spacing to 2 wide in ClusterAnalysis and EnsembleOverview

--- client/src/components/ClusterAnalysis.js
+++ client/src/components/ClusterAnalysis.js
@@ -4,64 +4,60 @@ import React, { Component } from "react";
 import CustomizedDot from "./CustomizeDot";
 
 class ClusterAnalysis extends Component {
-    data02 = [
-        { x: 20, y: 60 },
-        { x: 40, y: 290},
-        { x: 90, y: 90 },
-        { x: 98, y: 50 },
-        { x: 80, y: 80 },
-        { x: 10, y: 22 },
-    ];
+  data02 = [
+    { x: 20, y: 60 },
+    { x: 40, y: 290},
+    { x: 90, y: 90 },
+    { x: 98, y: 50 },
+    { x: 80, y: 80 },
+    { x: 10, y: 22 },
+  ];
 
-    render () {
-        return (
-            <Container style={{ height: '80vh' }}>
-                <ResponsiveContainer width="100%" height={'50%'}>
-                    <ScatterChart
-                        margin={{
-                            top: 20,
-                            right: 10,
-                            bottom: 10,
-                            left: 15,
-                        }}
-                    >
-                        <CartesianGrid />
-                        <XAxis type="number" dataKey="x" name="stature" unit="cm" label={{ value: "Data1", offset: -8, position: 'insideBottomRight' }} />
-                        <YAxis type="number" dataKey="y" name="weight" unit="kg" label={{ value: "Data2", offset: -2, angle: -90, position: 'insideBottomLeft' }} />
-                        {/* <ZAxis type="number" dataKey="z" scale="linear" range={[0, 500]} name="score" unit="km" /> */}
-                        <Tooltip cursor={{ strokeDasharray: '3 3' }} />
-                        <Legend />
-                        <Scatter name="District Plan" onClick={() => { console.log("Clicktest") }} data={this.data02} fill="#d88478" shape={<CustomizedDot />} />
-                    </ScatterChart>
-                </ResponsiveContainer>
-    
-    
-                <div style={{minHeight: '30%' }}>
-                    <Table striped style={{}}>
-                        <thead>
-                            <tr>
-                                
-                                <th>Specific Cluster Data 1</th>
-                                <th>Specific Cluster Data 2</th>
-                                <th>Specific Cluster Data 3</th>
-                                <th>Specific Cluster Data 4</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {/* Some map function for the list of dummy data */}
-                            <tr>
-                                
-                                <td>Info1</td>
-                                <td>Info2</td>
-                                <td>Info3</td>
-                                <td>Info4</td>
-                            </tr>
-                        </tbody>
-                    </Table>
-                </div>
-            </Container>
-        );
-    }
+  render () {
+    return (
+      <Container style={{ height: '80vh' }}>
+        <ResponsiveContainer width="100%" height={'50%'}>
+          <ScatterChart
+            margin={{
+              top: 20,
+              right: 10,
+              bottom: 10,
+              left: 15,
+            }}
+          >
+            <CartesianGrid />
+            <XAxis type="number" dataKey="x" name="stature" unit="cm" label={{ value: "Data1", offset: -8, position: 'insideBottomRight' }} />
+            <YAxis type="number" dataKey="y" name="weight" unit="kg" label={{ value: "Data2", offset: -2, angle: -90, position: 'insideBottomLeft' }} />
+            {/* <ZAxis type="number" dataKey="z" scale="linear" range={[0, 500]} name="score" unit="km" /> */}
+            <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+            <Legend />
+            <Scatter name="District Plan" onClick={() => { console.log("Clicktest") }} data={this.data02} fill="#d88478" shape={<CustomizedDot />} />
+          </ScatterChart>
+        </ResponsiveContainer>
+        <div style={{minHeight: '30%' }}>
+          <Table striped style={{}}>
+            <thead>
+              <tr>
+                <th>Specific Cluster Data 1</th>
+                <th>Specific Cluster Data 2</th>
+                <th>Specific Cluster Data 3</th>
+                <th>Specific Cluster Data 4</th>
+              </tr>
+            </thead>
+            <tbody>
+              {/* Some map function for the list of dummy data */}
+              <tr>
+                <td>Info1</td>
+                <td>Info2</td>
+                <td>Info3</td>
+                <td>Info4</td>
+              </tr>
+            </tbody>
+          </Table>
+        </div>
+      </Container>
+    );
+  }
 }
 
 export default ClusterAnalysis;
--- client/src/components/EnsembleOverview.js
+++ client/src/components/EnsembleOverview.js
@@ -4,69 +4,69 @@ import React, { Component } from "react";
 import CustomizedDot from "./CustomizeDot";
 
 class EnsembleOverview extends Component {
-    data01 = [
-        { x: 100, y: 200, z: 10 },
-        { x: 120, y: 100, z: 50 },
-        { x: 170, y: 300, z: 50 },
-        { x: 140, y: 250, z: 70 },
-        { x: 150, y: 400, z: 10 },
-        { x: 110, y: 280, z: 40 },
-    ];
+  data01 = [
+    { x: 100, y: 200, z: 10 },
+    { x: 120, y: 100, z: 50 },
+    { x: 170, y: 300, z: 50 },
+    { x: 140, y: 250, z: 70 },
+    { x: 150, y: 400, z: 10 },
+    { x: 110, y: 280, z: 40 },
+  ];
 
-    render () {
-        return (
-            <Container style={{ height: '80vh' }}>
-                <ResponsiveContainer width="100%" height={'50%'}>
-                    <ScatterChart
-                        margin={{
-                            top: 20,
-                            right: 10,
-                            bottom: 10,
-                            left: 15,
-                        }}
-                    >
-                        <CartesianGrid />
-                        <XAxis type="number" dataKey="x" name="stature" unit="cm" label={{ value: "Data1", offset: -8, position: 'insideBottomRight' }} />
-                        <YAxis type="number" dataKey="y" name="weight" unit="kg" label={{ value: "Data2", offset: -2, angle: -90, position: 'insideBottomLeft' }} />
-                        {/* <ZAxis type="number" dataKey="z" scale="linear" range={[0, 500]} name="score" unit="km" /> */}
-                        <Tooltip cursor={{ strokeDasharray: '3 3' }} />
-                        <Legend />
-                        <Scatter name="Clusters" onClick={() => { console.log("Clicktest") }} data={this.data01} fill="#8884d8" shape={<CustomizedDot />} />
-                    </ScatterChart>
-                </ResponsiveContainer>
-                <div style={{ overflowY: 'auto', minHeight: '30%' }}>
-                    <Table striped style={{}}>
-                        <thead>
-                            <tr>
-                                <th>ID</th>
-                                <th>General Cluster Data 1</th>
-                                <th>General Cluster Data 2</th>
-                                <th>General Cluster Data 3</th>
-                                <th>General Cluster Data 4</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {/* Some map function for the list of dummy data */}
-                            <tr>
-                                <td><Button variant="link">1</Button></td>
-                                <td>Info1</td>
-                                <td>Info2</td>
-                                <td>Info3</td>
-                                <td>Info4</td>
-                            </tr>
-                            <tr>
-                                <td><Button variant="link">2</Button></td>
-                                <td>Info1</td>
-                                <td>Info2</td>
-                                <td>Info3</td>
-                                <td>Info4</td>
-                            </tr>
-                        </tbody>
-                    </Table>
-                </div>
-            </Container>
-        );
-    }
+  render () {
+    return (
+      <Container style={{ height: '80vh' }}>
+        <ResponsiveContainer width="100%" height={'50%'}>
+          <ScatterChart
+            margin={{
+              top: 20,
+              right: 10,
+              bottom: 10,
+              left: 15,
+            }}
+          >
+            <CartesianGrid />
+            <XAxis type="number" dataKey="x" name="stature" unit="cm" label={{ value: "Data1", offset: -8, position: 'insideBottomRight' }} />
+            <YAxis type="number" dataKey="y" name="weight" unit="kg" label={{ value: "Data2", offset: -2, angle: -90, position: 'insideBottomLeft' }} />
+            {/* <ZAxis type="number" dataKey="z" scale="linear" range={[0, 500]} name="score" unit="km" /> */}
+            <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+            <Legend />
+            <Scatter name="Clusters" onClick={() => { console.log("Clicktest") }} data={this.data01} fill="#8884d8" shape={<CustomizedDot />} />
+          </ScatterChart>
+        </ResponsiveContainer>
+        <div style={{ overflowY: 'auto', minHeight: '30%' }}>
+          <Table striped style={{}}>
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>General Cluster Data 1</th>
+                <th>General Cluster Data 2</th>
+                <th>General Cluster Data 3</th>
+                <th>General Cluster Data 4</th>
+              </tr>
+            </thead>
+            <tbody>
+              {/* Some map function for the list of dummy data */}
+              <tr>
+                <td><Button variant="link">1</Button></td>
+                <td>Info1</td>
+                <td>Info2</td>
+                <td>Info3</td>
+                <td>Info4</td>
+              </tr>
+              <tr>
+                <td><Button variant="link">2</Button></td>
+                <td>Info1</td>
+                <td>Info2</td>
+                <td>Info3</td>
+                <td>Info4</td>
+              </tr>
+            </tbody>
+          </Table>
+        </div>
+      </Container>
+    );
+  }
 }
 
 export default EnsembleOverview;
