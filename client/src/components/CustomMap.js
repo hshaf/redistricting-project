16b085a9110b1ad3da0ea175ac58efb5296b2de3
Hamza Shafiq <53669917+hshaf@users.noreply.github.com>
2023-10-01 05:03:52 -0400

    clicking states now shows current district plan boundaries

--- client/src/components/CustomMap.js
+++ client/src/components/CustomMap.js
@@ -3,9 +3,9 @@ import React, { Component } from "react";
 import VAStateBoundaries from '../data/va-state-boundary.json';
 import AZStateBoundaries from '../data/az-state-boundary.json';
 import WIStateBoundaries from '../data/wi-state-boundary.json';
-// import VADistricts from '../data/va-districts.json';
-// import AZDistricts from '../data/az-districts.json';
-// import WIDistricts from '../data/wi-districts.json';
+import VADistricts from '../data/va-districts.json';
+import AZDistricts from '../data/az-districts.json';
+import WIDistricts from '../data/wi-districts.json';
 import ChangeView from './ChangeView';
 
 class CustomMap extends Component {
@@ -15,6 +15,15 @@ class CustomMap extends Component {
     this.mapCenter = [38.86438706880524, -95.47162384288428];
     // Current zoom level
     this.mapZoom = 7;
+    /* Boundary data for states. Can currently either show state 
+    or district (federal, senate, or house) boundaries. By default
+    we show the state boundaries. */
+    this.vaBoundariesData = VAStateBoundaries;
+    this.azBoundariesData = AZStateBoundaries;
+    this.wiBoundariesData = WIStateBoundaries;
+    /* To change the overlay on an existing GeoJSON object, we can
+    change the key value to force the object to rerender */
+    this.keyCount = 0;
   }
 
   render () {
@@ -23,6 +32,10 @@ class CustomMap extends Component {
       this.mapCenter = [37.47812615585515, -78.88801623378961];
       // Update zoom level
       this.mapZoom = 7;
+      // Update boundary data for states
+      this.vaBoundariesData = VADistricts;
+      this.azBoundariesData = AZStateBoundaries;
+      this.wiBoundariesData = WIStateBoundaries;
     }
 
     else if (this.props.selectedState == "AZ") {
@@ -30,6 +43,10 @@ class CustomMap extends Component {
       this.mapCenter = [34.35920229576733, -111.82765189051278];
       // Update zoom level
       this.mapZoom = 7;
+      // Update boundary data for states
+      this.vaBoundariesData = VAStateBoundaries;
+      this.azBoundariesData = AZDistricts;
+      this.wiBoundariesData = WIStateBoundaries;
     }
 
     else if (this.props.selectedState == "WI") {
@@ -37,6 +54,10 @@ class CustomMap extends Component {
       this.mapCenter = [44.61389658316453, -89.67045816895208];
       // Update zoom level
       this.mapZoom = 7;
+      // Update boundary data for states
+      this.vaBoundariesData = VAStateBoundaries;
+      this.azBoundariesData = AZStateBoundaries;
+      this.wiBoundariesData = WIDistricts;
     }
 
     else {
@@ -44,7 +65,13 @@ class CustomMap extends Component {
       this.mapCenter = [38.86438706880524, -95.47162384288428];
       // Update zoom level
       this.mapZoom = 5;
+      // Update boundary data for states
+      this.vaBoundariesData = VAStateBoundaries;
+      this.azBoundariesData = AZStateBoundaries;
+      this.wiBoundariesData = WIStateBoundaries;
     }
+
+    this.keyCount += 1;
     
     return (
       <MapContainer center={this.mapCenter} zoom={this.mapZoom} scrollWheelZoom={true}>
@@ -56,8 +83,8 @@ class CustomMap extends Component {
         <GeoJSON 
         weight={1} 
         color='blue' 
-        key={'Virginia'} 
-        data={VAStateBoundaries} 
+        key={'Virginia' + this.keyCount} 
+        data={this.vaBoundariesData} 
         eventHandlers={{
           click: () => {
             this.props.updateSelectedState("VA");
@@ -67,8 +94,8 @@ class CustomMap extends Component {
         <GeoJSON 
         weight={1} 
         color='blue' 
-        key={'Arizona'} 
-        data={AZStateBoundaries}
+        key={'Arizona' + this.keyCount} 
+        data={this.azBoundariesData}
         eventHandlers={{
           click: () => {
             this.props.updateSelectedState("AZ");
@@ -78,8 +105,8 @@ class CustomMap extends Component {
         <GeoJSON 
         weight={1} 
         color='blue' 
-        key={'Wisconsin'} 
-        data={WIStateBoundaries} 
+        key={'Wisconsin' + this.keyCount} 
+        data={this.wiBoundariesData} 
         eventHandlers={{
           click: () => {
             this.props.updateSelectedState("WI");
