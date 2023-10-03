e7a6cdb5f06ef02e1b03f7865235ab9877124ada
Hamza Shafiq <53669917+hshaf@users.noreply.github.com>
2023-10-03 00:47:58 -0400

    add table and list of distance measures

--- client/src/components/DistanceMeasures.js
+++ client/src/components/DistanceMeasures.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Table, Container } from "react-bootstrap";
 
 class DistanceMeasures extends Component {
   render () {
@@ -8,8 +9,57 @@ class DistanceMeasures extends Component {
       );
     }
 
+    var clusterData = Object.values(this.props.ensembleData[this.props.selectedState][this.props.selectedEnsembleID].clusters) // Change this to get data from request
+    const distanceTableEntries = clusterData.map((cluster) => {
+      const clusterNum = cluster["clusterNum"]
+      const clusterSize = cluster["count"]
+      const optimalTransport = cluster["distances"]["optimalTransport"];
+      const hamming = cluster["distances"]["hamming"];
+      const totalVariation = cluster["distances"]["totalVariation"];
+      return (
+        <tr key={`row-${clusterNum}`}>
+          <td>{clusterNum}</td>
+          <td>{clusterSize}</td>
+          <td>{optimalTransport.toFixed(3)}</td>
+          <td>{hamming.toFixed(3)}</td>
+          <td>{totalVariation.toFixed(3)}</td>
+        </tr>
+      )}
+    );
+
     return (
-      <h1>hello world</h1>
+      <Container style={{ height: '80vh' }}>
+        <h4>
+          List of Distance Measure Functions
+        </h4>
+        <Container>
+          <ul>
+            <li>Optimal Transport</li>
+            <li>Hamming Distance</li>
+            <li>Total Variation Distance</li>
+            <li>Centroid-Based Clustering</li>
+          </ul>
+        </Container>
+        <h4>
+          Cluster Distance Measures
+        </h4>
+        <div style={{ overflowY: 'auto', minHeight: '30%' }}>
+          <Table striped style={{}}>
+            <thead>
+              <tr>
+                <th>Cluster ID</th>
+                <th>Cluster Size</th>
+                <th>Optimal Transport</th>
+                <th>Hamming Distance</th>
+                <th>Total Variation Distance</th>
+              </tr>
+            </thead>
+            <tbody>
+              {distanceTableEntries}
+            </tbody>
+          </Table>
+        </div>
+      </Container>
     );
   }
 }
