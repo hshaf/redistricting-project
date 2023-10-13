5e2f411a08bc0e846b3333b3184c0c2b3434a9d2
Jaxaria <jakehoffman1160@gmail.com>
2023-10-13 17:12:33 -0400

    Changed dependencies to prepare for switch to mongo

--- server/RedistrictConnector/src/main/java/data/connector/RedistrictConnector/Models/District.java
+++ server/RedistrictConnector/src/main/java/data/connector/RedistrictConnector/Models/District.java
@@ -2,24 +2,13 @@ package data.connector.RedistrictConnector.Models;
 
 import java.util.List;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
 
-@Entity
 public class District {
   
-  @Id
-  @GeneratedValue(strategy = GenerationType.AUTO)
   private Integer id;
   
-  @ManyToOne(fetch = FetchType.LAZY)
   private Ensemble ensemble;
 
-  @ManyToOne(fetch = FetchType.LAZY)
   private Cluster cluster;
 
   protected District() {}
