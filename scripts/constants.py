14a3a5dcbbd1d192312806c25f60e16340db9e98
mapang02 <marcus.pang@stonybrook.edu>
2023-11-10 22:37:27 -0500

    Saving WIP preprocessing scripts

--- scripts/constants.py
+++ scripts/constants.py
@@ -1,16 +1,25 @@
 # Paths/values referenced across multiple files should be put here
 
-# Paths to data files
-VA_HOUSE_DISTRICTS_PATH = "../client/src/data/va_sldl_adopted_2021.zip"
-VA_DEMOGRAPHIC_PATH = "../client/src/data/va_pl2020_vtd.zip"
-VA_ELECTION_PATH = "../client/src/data/va_vest_20.zip"
-
+# Paths to data file
 AZ_HOUSE_DISTRICTS_PATH = "../client/src/data/az_sl_adopted_2022.zip"
 AZ_DEMOGRAPHIC_PATH = "../client/src/data/az_pl2020_vtd.zip"
 AZ_ELECTION_PATH = "../client/src/data/az_vest_20.zip"
 
+AZ_BLOCK_BOUNDARY_PATH = "../../Data/az_b_2020_bound.zip"
+AZ_BLOCK_ELECTION_PATH = "../../Data/az_2020_gen_2020_blocks.zip"
+
+VA_HOUSE_DISTRICTS_PATH = "../client/src/data/va_sldl_adopted_2021.zip"
+VA_DEMOGRAPHIC_PATH = "../client/src/data/va_pl2020_vtd.zip"
+VA_ELECTION_PATH = "../client/src/data/va_vest_20.zip"
+
+VA_BLOCK_BOUNDARY_PATH = "../../Data/va_b_2020_bound.zip"
+VA_BLOCK_ELECTION_PATH = "../../Data/va_2020_gen_2020_blocks.zip"
+
 WI_SENATE_DISTRICTS_PATH = "../client/src/data/wi_sldu_adopted_2022.zip"
 WI_DEMOGRAPHIC_PATH = "../client/src/data/wi_pl2020_vtd.zip"
 WI_ELECTION_PATH = "../client/src/data/wi_vest_20.zip"
+
+WI_BLOCK_BOUNDARY_PATH = "../../Data/wi_b_2020_bound.zip"
+WI_BLOCK_ELECTION_PATH = "../../Data/va_2020_gen_2020_blocks.zip"
 
 ENSEMBLE_DATA_JSON = "../client/src/data/updated-ensemble-data.json"
