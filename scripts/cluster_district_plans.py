2c3bb60fe153ee71dc91917e35217018c3560f9a
mapang02 <marcus.pang@stonybrook.edu>
2023-11-29 23:54:57 -0500

    Clean up file, add stub for Hamming distance

--- scripts/cluster_district_plans.py
+++ scripts/cluster_district_plans.py
@@ -12,30 +12,12 @@ from OptimalTransport import Pair
 # Configuration variables
 DATA_BASE_DIRECTORY = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "data")
 GEOPANDAS_ENGINE = "fiona"
-NUM_PROCESSES = 20
-
-def load_district_plans(prec_data_path: str, prec_adj_path: str, district_plan_files: list):
-    # Load graph
-    precinct_df = geopandas.read_file(prec_data_path, engine=GEOPANDAS_ENGINE)
-    with open(prec_adj_path, mode="r", encoding="utf-8") as adj_data_file:
-        edge_list = json.load(adj_data_file)
-        
-    graph = Graph(edge_list)
-    graph.add_data(precinct_df.set_index("vtd_geo_id")[["pop_total"]])
-
-    # Load plans
-    output = list()
-    for file in district_plan_files:
-        with open(file, mode="r", encoding="utf-8") as f:
-            assign_dict = json.load(f)
-            output.append(Partition(graph=graph, assignment=assign_dict))
-    return output
+NUM_PROCESSES = os.cpu_count()
 
 def load_graph(prec_data_path: str, prec_adj_path: str):
     precinct_df = geopandas.read_file(prec_data_path, engine=GEOPANDAS_ENGINE)
     with open(prec_adj_path, mode="r", encoding="utf-8") as adj_data_file:
         edge_list = json.load(adj_data_file)
-        
     graph = Graph(edge_list)
     graph.add_data(precinct_df.set_index("vtd_geo_id")[["pop_total"]])
     return graph
@@ -48,40 +30,14 @@ def load_assign_dicts(district_plan_files: list):
             output.append(assign_dict)
     return output
 
-def test_distance_func(plans: tuple):
-    return hash(plans[0]) / hash(plans[1])
+def calculate_dummy_distance(plan_1: Partition, plan_2: Partition):
+    return hash(plan_1) / hash(plan_2)
 
-def calculate_optimal_transport_distance(plans: tuple):
-    return Pair(plans[0], plans[1]).distance
+def calculate_optimal_transport_distance(plan_1: Partition, plan_2: Partition):
+    return Pair(plan_1, plan_2).distance
 
-def calculate_distances(district_plans: list, distance_func):
-    # Calculate distances
-    pair_indices = list()
-    for i in range(len(district_plans)):
-        for j in range(i + 1, len(district_plans)):
-            pair_indices.append((i, j))
-    district_plan_pairs = [(district_plans[p_1], district_plans[p_2]) for p_1, p_2 in pair_indices]
-
-    #p = mp.Pool(processes=NUM_PROCESSES)
-    #distances = p.map(distance_func, district_plan_pairs, chunksize=(len(pair_indices) // NUM_PROCESSES))
-    #p.close()
-    #p.join()
-    #distances = [distance_func(p) for p in district_plan_pairs]
-    with mp.Pool(processes=NUM_PROCESSES) as p:
-        distances = p.map(hash, district_plan_pairs)
-
-    # Organize distances into matrix
-    distance_matrix = np.zeros((len(district_plans), len(district_plans)), dtype=float)
-    for pair, distance in zip(pair_indices, distances):
-        distance_matrix[pair[0], pair[1]] = distance
-        distance_matrix[pair[1], pair[0]] = distance
-    print(distance_matrix)
-
-    # Normalize distances
-    max_distance = distance_matrix.max()
-    distance_matrix = distance_matrix / max_distance
-
-    return distance_matrix
+def calculate_hamming_distance(plan_1: Partition, plan_2: Partition):
+    return hash(plan_1) / hash(plan_2)
 
 def calculate_distances_runner(prec_data_path: str, 
                                 prec_adj_path: str, 
@@ -107,7 +63,6 @@ def calculate_distances_runner(prec_data_path: str,
     worker_args = [(graph, assign_dicts, p, distance_func) for p in per_worker_pairs]
     with mp.Pool(processes=NUM_PROCESSES) as p:
         distances = p.map(calculate_distances_worker, worker_args)
-    #distances = [calculate_distances_worker(arg_tuple) for arg_tuple in worker_args]
 
     # Organize distances into matrix
     distance_matrix = np.zeros((num_plans, num_plans), dtype=float)
@@ -115,7 +70,7 @@ def calculate_distances_runner(prec_data_path: str,
         for p1, p2, distance in distance_list:
             distance_matrix[p1, p2] = distance
             distance_matrix[p2, p1] = distance
-    print(distance_matrix)
+    #print(distance_matrix)
 
     # Normalize distances
     max_distance = distance_matrix.max()
@@ -141,7 +96,7 @@ def calculate_distances_worker(args):
     # Calculate distances
     output = list()
     for pair in district_plan_pairs:
-        distance = distance_func((plan_cache[pair[0]], plan_cache[pair[1]]))
+        distance = distance_func(plan_cache[pair[0]], plan_cache[pair[1]])
         output.append((pair[0], pair[1], distance))
     return output
 
@@ -177,22 +132,20 @@ def main():
             print("--plan-dir must be specified when calculating distances")
             exit(-1)
         plan_dir = os.path.join(DATA_BASE_DIRECTORY, args["plan_dir"])
-
         district_plan_files = sorted(os.path.join(plan_dir, f) for f in os.listdir(plan_dir))
-        #district_plans = load_district_plans(prec_data_path=prec_data_path, prec_adj_path=prec_adj_path, district_plan_files=district_plan_files)
 
+        # Select distance measure
         distance_func = calculate_optimal_transport_distance
         if args["distance_metric"] in ["o", "optimal_transport"]:
             distance_func = calculate_optimal_transport_distance
 
-        #distance_matrix = calculate_distances(district_plans, distance_func)
+        # Calculate distances
         distance_matrix = calculate_distances_runner(prec_data_path=prec_data_path,
                                                         prec_adj_path=prec_adj_path,
                                                         district_plan_files=district_plan_files,
-                                                        distance_func=test_distance_func
+                                                        distance_func=distance_func
                                                     )
 
-        # Save distances to file
         if args["distance_output_path"]:
             np.savetxt(distance_output_path, distance_matrix, delimiter=",")
 
