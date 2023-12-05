33106fa5899e14a4934e58624a2b5b2d7a84ee36
mapang02 <marcus.pang@stonybrook.edu>
2023-12-04 19:18:03 -0500

    Updating map to load data from server, WIP

--- client/src/components/CustomMap.js
+++ client/src/components/CustomMap.js
@@ -1,81 +1,131 @@
-import { MapContainer, TileLayer, GeoJSON } from 'react-leaflet';
-import { useRef, useContext } from "react";
-import VAStateBoundaries from '../data/va-state-boundary.json';
-import AZStateBoundaries from '../data/az-state-boundary.json';
-import WIStateBoundaries from '../data/wi-state-boundary.json';
-import VADistricts from '../data/va-districts.json';
+import { useContext, useEffect, useRef, useState } from "react";
+import { GeoJSON, MapContainer, TileLayer } from 'react-leaflet';
+import { AppStateActionType, AppStateContext, AppStateDispatch } from '../context/AppStateContext';
+import { AppDataContext, AppDataDispatch } from "../context/AppDataContext";
 import AZDistricts from '../data/az-districts.json';
+import AZStateBoundaries from '../data/az-state-boundary.json';
+import VADistricts from '../data/va-districts.json';
+import VAStateBoundaries from '../data/va-state-boundary.json';
 import WIDistricts from '../data/wi-districts.json';
+import WIStateBoundaries from '../data/wi-state-boundary.json';
 import ChangeView from './ChangeView';
-import { AppStateContext, AppStateDispatch, AppStateActionType } from '../context/AppStateContext';
-import { AppDataDispatch } from '../context/AppDataContext';
+import serverAPI from '../serverAPI'
+
+const defaultMapCenter = [38.86438706880524, -95.47162384288428];
+const defaultMapZoom = 7;
 
 export default function CustomMap(props) {
   // Context
   const appState = useContext(AppStateContext);
   const appStateDispatch = useContext(AppStateDispatch);
-
+  const appData = useContext(AppDataContext);
   const dataAPI = useContext(AppDataDispatch);
 
-  // Center of map coordinates
-  let mapCenter = [38.86438706880524, -95.47162384288428];
-  // Current zoom level
-  let mapZoom = 7;
-  /* Boundary data for states. Can currently either show state 
-  or district (federal, senate, or house) boundaries. By default
-  we show the state boundaries. */
-  let vaBoundariesData = VAStateBoundaries;
-  let azBoundariesData = AZStateBoundaries;
-  let wiBoundariesData = WIStateBoundaries;
+  const [boundary, setBoundary] = useState(null);
+
   /* To change the overlay on an existing GeoJSON object, we can
   change the key value to force the object to re-render.
   It is saved with useRef so it is persistent. */
   let keyCount = useRef(0);
   keyCount.current = keyCount.current + 1;
 
-  if (appState.selectedState === "VA") {
-    // Update map center over Virginia
-    mapCenter = [37.47812615585515, -78.88801623378961];
-    // Update zoom level
-    mapZoom = 7;
-    // Update boundary data for states
-    vaBoundariesData = VADistricts;
-    azBoundariesData = AZStateBoundaries;
-    wiBoundariesData = WIStateBoundaries;
-  }
+  // Center of map coordinates
+  let mapCenter = appState.selectedState ? appData.stateData.get(appState.selectedState)["mapCenter"] : defaultMapCenter;
+  let mapZoom = appState.selectedState ? appData.stateData.get(appState.selectedState)["mapZoom"] : defaultMapZoom;
 
-  else if (appState.selectedState === "AZ") {
-    // Update map center over Arizona
-    mapCenter = [34.35920229576733, -111.82765189051278];
-    // Update zoom level
-    mapZoom = 7;
-    // Update boundary data for states
-    vaBoundariesData = VAStateBoundaries;
-    azBoundariesData = AZDistricts;
-    wiBoundariesData = WIStateBoundaries;
-  }
+  let stateGeoJSONs = [...appData.stateData.values()].map((stateEntry) => {
+    console.log(stateEntry);
+    const stateInitials = stateEntry["initials"];
 
-  else if (appState.selectedState === "WI") {
-    // Update map center over Wisconsin
-    mapCenter = [44.61389658316453, -89.67045816895208];
-    // Update zoom level
-    mapZoom = 7;
-    // Update boundary data for states
-    vaBoundariesData = VAStateBoundaries;
-    azBoundariesData = AZStateBoundaries;
-    wiBoundariesData = WIDistricts;
-  }
+    // Set map zoom and center
+    mapCenter = stateEntry["mapCenter"];
+    mapZoom = stateEntry["mapZoom"];
 
-  else {
-    // Reset map center
-    mapCenter = [38.86438706880524, -95.47162384288428];
-    // Update zoom level
-    mapZoom = 5;
-    // Update boundary data for states
-    vaBoundariesData = VAStateBoundaries;
-    azBoundariesData = AZStateBoundaries;
-    wiBoundariesData = WIStateBoundaries;
+    // Retrieve state boundary and current district plan map
+    let stateBoundaries = appData.stateBoundaries.get(stateInitials);
+    if (appData.selectedState === stateInitials) {
+      if (boundary === null) {
+        stateBoundaries = appData.currDistrictPlans.get(stateInitials);
+      }
+      else {
+        stateBoundaries = boundary;
+      }
+    }
+
+    return (
+      <GeoJSON 
+        weight={1} 
+        color='blue' 
+        key={stateInitials + keyCount.current} 
+        data={JSON.parse(stateBoundaries)} 
+        eventHandlers={{
+          click: () => {
+            appStateDispatch({
+              type: AppStateActionType.SET_SELECTED_STATE,
+              payload: stateInitials
+            })
+            dataAPI.getEnsemblesForState(stateInitials);
+          }
+        }} 
+      />
+    );
+  })
+  /*
+  for (let stateEntry in appData.stateData.entries()) {
+    console.log(stateEntry);
+    const stateInitials = stateEntry.key;
+    const stateData = stateEntry.value;
+
+    // Set map zoom and center
+    mapCenter = stateData["mapCenter"];
+    mapZoom = stateData["mapZoom"];
+
+    // Retrieve state boundary and current district plan map
+    let stateBoundaries = appData.stateBoundaries.get(stateInitials);
+    if (appData.selectedState === stateInitials) {
+      stateBoundaries = appData.currDistrictPlans.get(stateInitials);
+    }
+    if (boundary !== null) {
+      stateBoundaries = boundary;
+    }
+
+    stateGeoJSONs.push(
+      <GeoJSON 
+        weight={1} 
+        color='blue' 
+        key={stateInitials + keyCount.current} 
+        data={JSON.parse(stateBoundaries)} 
+        eventHandlers={{
+          click: () => {
+            appStateDispatch({
+              type: AppStateActionType.SET_SELECTED_STATE,
+              payload: stateInitials
+            })
+            dataAPI.getEnsemblesForState(stateInitials);
+          }
+        }} 
+      />
+    );
   }
+  */
+  console.log(stateGeoJSONs);
+
+  // Retrieve boundary when selected cluster changes
+  let getBoundary = async (boundaryID) => {
+    console.log("Retrieve boundary " + boundaryID);
+    const response = await serverAPI.getBoundaryById(boundaryID);
+    if (response.status === 200) {
+      setBoundary(response.data.data);
+    }
+  }
+  useEffect(() => {
+    if (appState.selectedClusterID) {
+      getBoundary(appData.selectedEnsembleClusters[appState.selectedClusterID]["boundary"]);
+    }
+    else {
+      setBoundary(null);
+    }
+  }, [appData.selectedEnsembleClusters, appState.selectedClusterID]);
 
   // Render CustomMap
   return (
@@ -85,54 +135,7 @@ export default function CustomMap(props) {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <GeoJSON 
-        weight={1} 
-        color='blue' 
-        key={'Virginia' + keyCount.current} 
-        data={vaBoundariesData} 
-        eventHandlers={{
-          click: () => {
-            appStateDispatch({
-              type: AppStateActionType.SET_SELECTED_STATE,
-              payload: "VA"
-            })
-
-            dataAPI.getEnsemblesForState("VA");
-          }
-        }} 
-      />
-      <GeoJSON 
-        weight={1} 
-        color='blue' 
-        key={'Arizona' + keyCount.current} 
-        data={azBoundariesData}
-        eventHandlers={{
-          click: () => {
-            appStateDispatch({
-              type: AppStateActionType.SET_SELECTED_STATE,
-              payload: "AZ"
-            })
-
-            dataAPI.getEnsemblesForState("AZ");
-          }
-        }}
-      />
-      <GeoJSON 
-        weight={1} 
-        color='blue' 
-        key={'Wisconsin' + keyCount.current} 
-        data={wiBoundariesData} 
-        eventHandlers={{
-          click: () => {
-            appStateDispatch({
-              type: AppStateActionType.SET_SELECTED_STATE,
-              payload: "WI"
-            })
-
-            dataAPI.getEnsemblesForState("WI");
-          }
-        }}  
-      />
+      {stateGeoJSONs}
     </MapContainer>
   );
 }
